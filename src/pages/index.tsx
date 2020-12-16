7f1b7321b3492f448d6b76224923e6cfb7aec864
Abdul Samad <Samk8778@yahoo.com>
2020-12-16 19:26:03 +0530

    :lipstick: Added theme colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { PageProps, graphql } from 'gatsby';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import theme from '../theme';
 
 type DataProps = {
   site: {
@@ -11,40 +11,13 @@ type DataProps = {
   };
 };
 
-const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <>
-    <Nav />
-    <Layout>
-      <SEO title='Using TypeScript' />
-      <h1>Gatsby supports TypeScript by default!</h1>
-      <p>
-        This means that you can create and write <em>.ts/.tsx</em> files for
-        your pages, components etc. Please note that the <em>gatsby-*.js</em>{' '}
-        files (like gatsby-node.js) currently don't support TypeScript yet.
-      </p>
-      <p>
-        For type checking you'll want to install <em>typescript</em> via npm and
-        run <em>tsc --init</em> to create a <em>.tsconfig</em> file.
-      </p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <p>
-        You're currently on the page "{path}" which was built on{' '}
-        {data.site.buildTime}.
-      </p>
-      <p>
-        To learn more, head over to our{' '}
-        <a href='https://www.gatsbyjs.com/docs/typescript/'>
-          documentation about TypeScript
-        </a>
-        .
-      </p>
-    </Layout>
-  </>
-);
-
-export default UsingTypescript;
+const IndexPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Normalize />
+    </ThemeProvider>
+  );
+};
 
 export const query = graphql`
   {
@@ -53,3 +26,5 @@ export const query = graphql`
     }
   }
 `;
+
+export default IndexPage;
---
+++ src/theme/index.tsx
@@ -0,0 +1,14 @@
+const theme = {
+  color: {
+    primary: '#2f89fc',
+    secondary: '#30e3ca',
+    light: '#f5f5f5',
+    dark: '#1f1f1f',
+  },
+  font: {
+    primary: 'Montserrat',
+    secondary: 'Lato',
+  },
+};
+
+export default theme;
