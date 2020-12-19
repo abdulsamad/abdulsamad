f88c2e13799bad727ab62a855e3d265f4ae3d440
Abdul Samad <Samk8778@yahoo.com>
2020-12-20 01:25:07 +0530

    :truck: Changed imported component name

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Navbar from './Header';
+import Header from './Header';
 import NavMenu from './NavMenu';
 import NavLink from './NavLink';
 import Logo from './Logo';
@@ -9,14 +9,14 @@ import Container from '../utils/Container';
 const Index: React.FC = () => {
   return (
     <Container>
-      <Navbar>
+      <Header>
         <Logo height={48} fill='#f5f5f5' />
         <NavMenu>
           <NavLink href='#skills'>Skills</NavLink>
           <NavLink href='#projects'>Projects</NavLink>
           <NavLink href='#contact'>Contact</NavLink>
         </NavMenu>
-      </Navbar>
+      </Header>
     </Container>
   );
 };
