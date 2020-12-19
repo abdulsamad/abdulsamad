3107358fea3464a276289ddf0e5158a6d3c30be2
Abdul Samad <Samk8778@yahoo.com>
2020-12-20 00:55:43 +0530

    :lipstick: Added background blur and changed link fons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,6 +9,12 @@ const Header = styled.header`
   justify-content: space-between;
   box-sizing: border-box;
   background: transparent;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  backdrop-filter: blur(60px);
+  z-index: 10;
 `;
 
 export default Header;
