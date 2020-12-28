e0ca76512a399c67dba49394404c9178437ac85b
Abdul Samad <Samk8778@yahoo.com>
2020-12-28 20:15:51 +0530

    :construction: Updated header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 const Header = styled.header`
   height: 60px;
   max-width: 100%;
-  padding: 2rem;
+  padding: 2rem 5vw;
   display: flex;
   align-items: center;
   justify-content: space-between;
   box-sizing: border-box;
-  background: transparent;
-  position: fixed;
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 20px 30px 50px rgba(0, 0, 0, 0.5);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  position: absolute;
   top: 0;
   left: 0;
   right: 0;
