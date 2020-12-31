40e9ef03eb0cc7df519611222e4297769f36b42c
Abdul Samad <Samk8778@yahoo.com>
2020-12-31 19:30:00 +0530

    :lipstick: Added List component

---
+++ src/components/skills/List.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const List = styled.ul`
+  list-style-type: none;
+  list-style-type: circle;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-weight: 600;
+`;
+
+export default List;
