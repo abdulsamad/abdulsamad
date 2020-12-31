118d1344d27c4f4d157b59a177d6e484891d81b3
Abdul Samad <Samk8778@yahoo.com>
2020-12-31 19:27:57 +0530

    :lipstick: Added hero component

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Hero = styled.div`
+  position: relative;
+  height: 100vh;
+  max-width: 100vw;
+  overflow: hidden;
+  padding-top: 60px;
+`;
+
+export default Hero;
