1545efae9a2012e048680afa20a13402f63fe93f
Abdul Samad <Samk8778@yahoo.com>
2020-12-31 22:07:53 +0530

    :lipstick: Added Input componet

---
+++ src/components/contact/Input.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  height: 2rem;
+  border-radius: 5px;
+  width: 50%;
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 5px;
+`;
+
+export default Input;
