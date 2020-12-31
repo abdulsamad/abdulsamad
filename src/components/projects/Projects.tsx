e0ec5717110d8de82d26029c5fb91c3543b22b4a
Abdul Samad <Samk8778@yahoo.com>
2020-12-31 21:28:18 +0530

    :lipstick: Added Projects component

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const Projects = styled.section`
-  height: 400px;
   background-color: #1f1f1f;
   padding: 2rem;
   text-align: center;
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -4,18 +4,61 @@ import Project from './Projects';
 import Card from './Card';
 import CardHeader from './CardHeader';
 import CardBody from './CardBody';
+import CardFooter from './CardFooter';
+import Link from './Link';
+import Container from './Container';
 
-const Index = () => {
+interface ProjectListTypes {
+  title: string;
+  description: string;
+  url: string;
+  source: string;
+}
+
+const projectsList: object[ProjectListTypes] = [
+  {
+    title: 'Peek History',
+    description:
+      'A simple and minimal extension for quickly peeking and managing history.',
+    url:
+      'https://chrome.google.com/webstore/detail/peek-history/gknodemjjckmkncijnedcpogffimkmbm&source=portfolio',
+    source: 'https://github.com/abdulsamad/peek_history',
+  },
+  {
+    title: 'WeatherX',
+    description: 'A clean and minimal weather PWA aim towards mobile.',
+    url: 'https://weatherx-abdulsamad.netlify.app&source=portfolio',
+    source: 'https://github.com/abdulsamad/weatherx',
+  },
+  {
+    title: 'Loan Calculator',
+    description:
+      "This program help's you to calculate the loan repayment amount.",
+    url: 'https://abdulsamad.github.io/loan_calculator&source=portfolio',
+    source: 'https://github.com/abdulsamad/loan_calculator',
+  },
+];
+
+const Index: React.FC = () => {
   return (
     <Project id='projects'>
       <h1>Projects</h1>
-      <Card>
-        <CardHeader>Peek History</CardHeader>
-        <CardBody>
-          A simple and minimal extension for quickly peeking and managing
-          history.
-        </CardBody>
-      </Card>
+      <Container>
+        {projectsList.map(({ title, description, url, source }) => (
+          <Card>
+            <CardHeader>{title}</CardHeader>
+            <CardBody>{description}</CardBody>
+            <CardFooter>
+              <Link href={url} target='_blank'>
+                Live
+              </Link>
+              <Link href={source} target='_blank'>
+                Source
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </Container>
     </Project>
   );
 };
