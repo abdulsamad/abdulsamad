82f99733dec053a3d9b8c321f52ea6387ae98a77
Abdul Samad <Samk8778@yahoo.com>
2020-12-31 19:31:30 +0530

    :lipstick: Added initial Shape utility component

---
+++ src/components/utils/Shapes.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface CircleProps {
+  height: string;
+  width: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+  background: string;
+}
+
+const Circle = styled.div<CircleProps>`
+  z-index: -1;
+  position: absolute;
+  top: ${({ top }) => top};
+  right: ${({ right }) => right};
+  bottom: ${({ bottom }) => bottom};
+  left: ${({ left }) => left};
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+  background: ${({ background }) => background};
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+interface ShapeProps {
+  type: 'circle';
+  height: string;
+  width: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+  background: string;
+}
+
+const Shapes: React.FC<ShapeProps> = ({
+  type,
+  height,
+  width,
+  top,
+  right,
+  bottom,
+  left,
+  background,
+}) => {
+  switch (type) {
+    default:
+      return (
+        <Circle
+          height={height}
+          width={width}
+          top={top}
+          right={right}
+          bottom={bottom}
+          left={left}
+          background={background}
+        />
+      );
+  }
+};
+
+export default Shapes;
