de80dc506b529e818bae90f5d7b2f253af4f2cc5
Abdul Samad <Samk8778@yahoo.com>
2021-01-01 17:18:22 +0530

    :wastebasket: Cleaned footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 
 import Footer from './Footer';
 
-interface Props {
-  path: string;
-  data: {
-    [key: string]: any;
-  };
-}
-
-const Index: React.FC<Props> = ({ path, data }) => {
+const Index: React.FC = () => {
   return <Footer>&copy; {new Date().getFullYear()} - abdulsamad.dev</Footer>;
 };
 
