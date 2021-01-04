5a37009454c59323566357b92d8a0082048d15a4
Abdul Samad <Samk8778@yahoo.com>
2021-01-04 22:23:07 +0530

    :lipstick: Added company component

---
+++ src/components/testimonials/Company.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Company = styled.div`
+  margin: 0.5rem;
+  text-align: right;
+  font-weight: 600;
+  font-style: italic;
+`;
+
+export default Company;
