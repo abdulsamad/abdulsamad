8eab4484ec549345671f290541c683c7bb9e06fc
Abdul Samad <Samk8778@yahoo.com>
2021-01-04 14:34:53 +0530

    :lipstick: Updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageProps, graphql } from 'gatsby';
 import { Normalize } from 'styled-normalize';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import theme from '../theme';
 import GlobalStyle from '../components/utils/GlobalStyle';
@@ -13,48 +13,22 @@ import Projects from '../components/projects';
 import Contact from '../components/contact';
 import Footer from '../components/footer';
 
-type DataProps = {
-  site: {
-    buildTime: string;
-  };
-};
-
-const Alert = styled.h2`
-  background-color: #2f89fc;
-  margin: 0;
-  padding: 10px;
-  text-align: center;
-  font-size: 1rem;
-`;
-
-const IndexPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
+const IndexPage: React.FC<PageProps> = () => {
   return (
     <>
       <Normalize />
-
       <SEO title='AbdulSamad Portfolio' />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Alert>
-          🚧 Website in under active development — Please visit again later! 🚧
-        </Alert>
         <Header />
         <Hero />
         <Skills />
         <Projects />
         <Contact />
-        <Footer path={path} data={data} />
+        <Footer />
       </ThemeProvider>
     </>
   );
 };
-
-export const query = graphql`
-  {
-    site {
-      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
-    }
-  }
-`;
 
 export default IndexPage;
