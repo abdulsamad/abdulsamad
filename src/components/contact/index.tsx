2e382a1e43fd459f8bf346a23694acb80332f531
Abdul Samad <Samk8778@yahoo.com>
2021-01-05 13:11:58 +0530

    :construction:  Updated Contact form

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -43,7 +43,7 @@ const index = () => {
             <span />
           </Field>
           <Field>
-            <ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_SITE_KEY} />
+            <ReCAPTCHA sitekey={process.env.SITE_RECAPTCHA_KEY} />
           </Field>
           <SubmitBtn type='submit'>Send</SubmitBtn>
         </Form>
