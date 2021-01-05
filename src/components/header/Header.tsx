4e9ed71b84705baf5dd7e990ffe07225c8ccbade
Abdul Samad <Samk8778@yahoo.com>
2021-01-05 13:23:02 +0530

    :lipstick: Fixed layering z-index issues

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,7 +15,7 @@ const Header = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  z-index: 10;
+  z-index: 20;
 `;
 
 export default Header;
--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -3,6 +3,8 @@ import Container from '../utils/Container';
 
 const HeroText = styled(Container)`
   margin: 200px 0 0 200px;
+  position: relative;
+  z-index: 10;
 
   @media (max-width: 768px) {
     margin: 100px 5vw;
