b86bb4303d6b6c64e6485e6c6230c44e75d15d8b
Abdul Samad <Samk8778@yahoo.com>
2021-01-05 17:22:11 +0530

    :lipstick: Improved card UI

--- src/components/testimonials/Card.tsx
+++ src/components/testimonials/Card.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 const Card = styled.div`
-  padding: 0 4rem;
+  padding: 2rem;
+  background: #1b1b1b;
+  margin-right: 1rem;
+  border-left: 5px solid ${({ theme }) => theme.color.primary};
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 
   @media (max-width: 768px) {
-    padding: 0;
+    padding: 1rem;
     margin: 2rem 0;
   }
 `;
