e4e98d2c1cd654bf54c92bc235f952747a077f2f
Abdul Samad <Samk8778@yahoo.com>
2021-01-05 19:31:59 +0530

    :construction: Updated Contact form

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -11,8 +11,32 @@ import Input from './Input';
 import Label from './Label';
 import SubmitBtn from './SubmitBtn';
 import Textarea from './TextArea';
+import { string } from 'prop-types';
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 
 const Index = () => {
+  const [formData, setFormData] = useState<{ [key: string]: any }>({});
+
+  const handleChange = (ev: React.ChangeEvent) =>
+    setFormData({ [ev.target.name]: ev.target.value });
+
+  const handleSubmit = (ev: React.FormEvent) => {
+    ev.preventDefault();
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formData }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <Contact id='contact'>
       <Container>
@@ -20,17 +44,28 @@ const Index = () => {
           name='contact'
           method='POST'
           data-netlify='true'
-          data-netlify-recaptcha='true'>
+          data-netlify-recaptcha='true'
+          onSubmit={handleSubmit}>
           {/* Netlify Form Field */}
           <input type='hidden' name='form-name' value='contact' />
           <FormHeading>Get in touch!</FormHeading>
           <Field>
-            <Input type='text' placeholder='Your Name' name='name' />
+            <Input
+              type='text'
+              placeholder='Your Name'
+              name='name'
+              onChange={handleChange}
+            />
             <Label>Name</Label>
             <span />
           </Field>
           <Field>
-            <Input type='email' placeholder='john@example.com' name='email' />
+            <Input
+              type='email'
+              placeholder='john@example.com'
+              name='email'
+              onChange={handleChange}
+            />
             <Label>Email</Label>
             <span />
           </Field>
@@ -39,12 +74,17 @@ const Index = () => {
               rows={5}
               placeholder='Enter your message here...'
               name='message'
+              onChange={handleChange}
             />
             <Label>Message</Label>
             <span />
           </Field>
           <Field>
-            <ReCAPTCHA sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} />
+            <ReCAPTCHA
+              style={{ margin: 'auto' }}
+              theme='dark'
+              sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY}
+            />
           </Field>
           <SubmitBtn type='submit'>Send</SubmitBtn>
         </Form>
