0531d8e732e5b2889188e52b0fe3520fff20a057
Abdul Samad <Samk8778@yahoo.com>
2021-01-06 11:40:45 +0530

    :lipstick: Updated ListItem style type

--- src/components/skills/ListItem.tsx
+++ src/components/skills/ListItem.tsx
@@ -1,16 +1,10 @@
 import styled from 'styled-components';
-import svg from '../../assets/arrow.svg';
 
 const ListItem = styled.li`
   margin: 0 4rem;
   white-space: nowrap;
   line-height: 3rem;
-  list-style-type: none;
-
-  &::before {
-    content: '➡️';
-    margin-right: 5px;
-  }
+  list-style-type: circle;
 `;
 
 export default ListItem;
