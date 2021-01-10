2bc90831b947785eb61315cbfd1e5d4538312df4
Abdul Samad <Samk8778@yahoo.com>
2021-01-10 12:04:57 +0530

    :art: Made skills component more modular

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -6,22 +6,28 @@ import List from './List';
 import ListItem from './ListItem';
 import Container from '../utils/Container';
 
-const Index = () => {
+const skills: string[] = [
+  'HTML5',
+  'CSS3',
+  'JavaScript',
+  'React',
+  'Git',
+  'Sass',
+  'Material UI',
+  'Bootstrap',
+  'Materialize CSS',
+  'Styled-Components',
+];
+
+const Index: React.FC = () => {
   return (
     <Skills id='skills'>
       <Heading>Skills</Heading>
       <Container>
         <List>
-          <ListItem>HTML5</ListItem>
-          <ListItem>CSS3</ListItem>
-          <ListItem>JavaScript</ListItem>
-          <ListItem>React</ListItem>
-          <ListItem>Git</ListItem>
-          <ListItem>Sass</ListItem>
-          <ListItem>Material UI</ListItem>
-          <ListItem>Bootstrap</ListItem>
-          <ListItem>Materialize CSS</ListItem>
-          <ListItem>Styled-Components</ListItem>
+          {skills.map((skill) => (
+            <ListItem>{skill}</ListItem>
+          ))}
         </List>
       </Container>
     </Skills>
