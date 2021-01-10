f897878cc21602309274d91db5833b6124edd508
Abdul Samad <Samk8778@yahoo.com>
2021-01-10 14:27:36 +0530

    :art: Added implicit return

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -11,28 +11,26 @@ import Container from './Container';
 
 import projectsList from './projectsList';
 
-const Index: React.FC = () => {
-  return (
-    <Project id='projects'>
-      <Heading>Projects</Heading>
-      <Container>
-        {projectsList.map(({ title, description, url, source }) => (
-          <Card key={url}>
-            <CardHeader>{title}</CardHeader>
-            <CardBody>{description}</CardBody>
-            <CardFooter>
-              <Link href={url} target='_blank'>
-                Live
-              </Link>
-              <Link href={source} target='_blank'>
-                Source
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </Container>
-    </Project>
-  );
-};
+const Index: React.FC = () => (
+  <Project id='projects'>
+    <Heading>Projects</Heading>
+    <Container>
+      {projectsList.map(({ title, description, url, source }) => (
+        <Card key={url}>
+          <CardHeader>{title}</CardHeader>
+          <CardBody>{description}</CardBody>
+          <CardFooter>
+            <Link href={url} target='_blank'>
+              Live
+            </Link>
+            <Link href={source} target='_blank'>
+              Source
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </Container>
+  </Project>
+);
 
 export default Index;
