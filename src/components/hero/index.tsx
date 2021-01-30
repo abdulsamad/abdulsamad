a1e57aa63f6f81064b39397ac46987c350d44f98
Abdul Samad <Samk8778@yahoo.com>
2021-01-30 19:57:59 +0530

    :dizzy: Switch to react-spring hooks for animation

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Spring } from 'react-spring/renderprops';
+import { useTrail, animated } from 'react-spring';
 import scrollTo from 'gatsby-plugin-smoothscroll';
 
 import Hero from './Hero';
@@ -9,54 +9,64 @@ import Intro from './Intro';
 import Message from './Message';
 import Button from '../utils/Button';
 
+const heroTextArr: React.ReactElement[] = [
+  <Intro>
+    Hi, I’m AbdulSamad, <br /> Front-End <span>Developer</span> &amp;
+  </Intro>,
+  <Message>
+    Your Technical Partner <br /> Towards Web Success
+  </Message>,
+];
+
 interface Props {
   title?: string;
 }
 
-const Index: React.FC<Props> = () => (
-  <Hero>
-    <Shapes
-      type='circle'
-      height='400px'
-      width='400px'
-      top='-100px'
-      left='-100px'
-      background='linear-gradient(to left, rgb(18, 194, 233), rgb(196, 113, 237), rgb(246, 79, 89))'
-    />
-    <Shapes
-      type='circle'
-      height='400px'
-      width='400px'
-      bottom='-100px'
-      right='-100px'
-      background='linear-gradient(to right, rgb(18, 194, 233), rgb(196, 113, 237), rgb(246, 79, 89))'
-    />
-    <HeroText>
-      <Spring
-        from={{ opacity: 0, transform: 'translateY(-10px)' }}
-        to={{ opacity: 1, transform: 'translateY(0)' }}>
-        {(props) => (
-          <Intro style={props}>
-            Hi, I’m AbdulSamad, <br /> Front-End <span>Developer</span> &amp;
-          </Intro>
-        )}
-      </Spring>
-      <Spring
-        from={{ opacity: 0, transform: 'translateY(-20px)' }}
-        to={{ opacity: 1, transform: 'translateY(0)' }}
-        delay={500}>
-        {(props) => (
-          <Message style={props}>
-            Your Technical Partner <br /> Towards Web Success
-          </Message>
-        )}
-      </Spring>
-      <div>
-        <Button onClick={() => scrollTo('#projects')}>View Projects</Button>
-        <Button onClick={() => scrollTo('#contact')}>Let&apos;s Talk</Button>
-      </div>
-    </HeroText>
-  </Hero>
-);
+const Index: React.FC<Props> = () => {
+  interface trailProps {
+    opacity: number;
+  }
+
+  const [trail, set] = useTrail(
+    heroTextArr.length,
+    (): trailProps => ({
+      opacity: 0,
+    })
+  );
+
+  set({ opacity: 1 });
+
+  return (
+    <Hero>
+      <Shapes
+        type='circle'
+        height='400px'
+        width='400px'
+        top='-100px'
+        left='-100px'
+        background='linear-gradient(to left, rgb(18, 194, 233), rgb(196, 113, 237), rgb(246, 79, 89))'
+      />
+      <Shapes
+        type='circle'
+        height='400px'
+        width='400px'
+        bottom='-100px'
+        right='-100px'
+        background='linear-gradient(to right, rgb(18, 194, 233), rgb(196, 113, 237), rgb(246, 79, 89))'
+      />
+      <HeroText>
+        {trail.map((props, index) => (
+          <animated.div style={props}>{heroTextArr[index]}</animated.div>
+        ))}
+        <div>
+          <Button onClick={() => scrollTo('#contact')}>Let&apos;s Talk</Button>
+          <Button outlined={true} onClick={() => scrollTo('#projects')}>
+            View Projects
+          </Button>
+        </div>
+      </HeroText>
+    </Hero>
+  );
+};
 
 export default Index;
