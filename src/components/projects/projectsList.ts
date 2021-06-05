64f558991d5131add6a6384ea4cb4da6ced011da
Abdul Samad <Samk8778@yahoo.com>
2021-06-05 16:14:29 +0530

    :speech_balloon: Updated project links

--- src/components/projects/projectsList.ts
+++ src/components/projects/projectsList.ts
@@ -2,36 +2,35 @@ interface ProjectListTypes {
   title: string;
   description: string;
   url: string;
-  source: string;
+  source_url: string;
 }
 
 const projectsList: ProjectListTypes[] = [
   {
     title: 'E-Commerce Project',
     description: 'An E-Commerce Store with admin controls',
-    url: 'https://github.com/abdulsamad/fashop/tree/deprecated',
-    source: 'https://github.com/abdulsamad/fashop/tree/deprecated',
+    url: 'https://fa-shop.herokuapp.com',
+    source_url: 'https://github.com/abdulsamad/fashop',
   },
   {
     title: 'Peek History',
     description:
       'A simple and minimal extension for quickly peeking and managing history.',
-    url:
-      'https://chrome.google.com/webstore/detail/peek-history/gknodemjjckmkncijnedcpogffimkmbm?source=portfolio',
-    source: 'https://github.com/abdulsamad/peek_history',
+    url: 'https://chrome.google.com/webstore/detail/peek-history/gknodemjjckmkncijnedcpogffimkmbm?source=portfolio',
+    source_url: 'https://github.com/abdulsamad/peek_history',
   },
   {
     title: 'WeatherX',
     description: 'A clean and minimal weather PWA aim towards mobile.',
     url: 'https://weatherx-abdulsamad.netlify.app?source=portfolio',
-    source: 'https://github.com/abdulsamad/weatherx',
+    source_url: 'https://github.com/abdulsamad/weatherx',
   },
   {
     title: 'Loan Calculator',
     description:
       "This program help's you to calculate the loan repayment amount.",
     url: 'https://abdulsamad.github.io/loan_calculator?source=portfolio',
-    source: 'https://github.com/abdulsamad/loan_calculator',
+    source_url: 'https://github.com/abdulsamad/loan_calculator',
   },
 ];
 
