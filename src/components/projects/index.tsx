cdc12d1bb46f3344c76cbaf3cfa5c3e5afc563ae
AbdulSamad <Samk8778@yahoo.com>
2021-07-15 21:25:33 +0530

    :dizzy: Added tilt 3D effect on project cards

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import VanillaTilt from 'vanilla-tilt';
 
 import Project from './Projects';
 import Heading from '../utils/Heading';
@@ -12,34 +13,44 @@ import GitHubLink from './GitHubLink';
 
 import projectsList from './projectsList';
 
-const Index: React.FC = () => (
-  <Project id='projects'>
-    <Heading>Projects</Heading>
-    <Container>
-      {projectsList.map(({ title, description, url, source_url }) => (
-        <Card key={url}>
-          <CardHeader>{title}</CardHeader>
-          <CardBody>{description}</CardBody>
-          <CardFooter>
-            <Link href={url} target='_blank' rel='noopener'>
-              Live
-            </Link>
-            <Link href={source_url} target='_blank' rel='noopener'>
-              Source
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
-    </Container>
-    <Container>
-      <GitHubLink
-        href='https://github.com/abdulsamad?tab=repositories&q=&type=source&language=&sort='
-        target='_blank'
-        rel='noopener'>
-        Checkout more on GitHub
-      </GitHubLink>
-    </Container>
-  </Project>
-);
+const Index: React.FC = () => {
+  const cardsRef = useRef<HTMLDivElement[] | null>([]);
+
+  useEffect(() => {
+    cardsRef.current.forEach((el: HTMLDivElement) => {
+      VanillaTilt.init(el);
+    });
+  }, []);
+
+  return (
+    <Project id='projects'>
+      <Heading>Projects</Heading>
+      <Container>
+        {projectsList.map(({ title, description, url, source_url }, index) => (
+          <Card key={url} ref={(el) => (cardsRef.current[index] = el)}>
+            <CardHeader>{title}</CardHeader>
+            <CardBody>{description}</CardBody>
+            <CardFooter>
+              <Link href={url} target='_blank' rel='noopener'>
+                Live
+              </Link>
+              <Link href={source_url} target='_blank' rel='noopener'>
+                Source
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </Container>
+      <Container>
+        <GitHubLink
+          href='https://github.com/abdulsamad?tab=repositories&q=&type=source&language=&sort='
+          target='_blank'
+          rel='noopener'>
+          Checkout more on GitHub
+        </GitHubLink>
+      </Container>
+    </Project>
+  );
+};
 
 export default Index;
