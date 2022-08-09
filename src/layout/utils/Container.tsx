542c8821ba8a28974a3051f44ad419511849ec2d
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-08-10 01:30:42 +0530

    :lipstick: Added utils

---
+++ src/layout/utils/Container.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid red;
+  box-sizing: border-box;
+  padding: auto 5vw auto 5vw;
+  height: 60px;
+  width: 100%;
+  max-width: 1920px;
+  /* margin: auto; */
+`;
+
+export default Container;
---
+++ src/layout/utils/GlobalStyle.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    scroll-behavior: smooth;
+  }
+  
+  body {
+    padding: 0;
+    margin: 0; 
+    color: ${({ theme }) => theme.color.text};
+    font-family: ${({ theme }) => theme.fontFamily.primary};
+  }
+
+  ::-webkit-scrollbar {
+    background-color: ${({ theme }) => theme.color.background};
+    border: none;
+  }v
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.color.primary};
+    border-radius: 10px;
+  }
+`;
+
+export default GlobalStyle;
