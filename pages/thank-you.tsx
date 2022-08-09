bb1554ac4032b7a909d59e23e31e4e8d11f4a724
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-08-10 00:31:26 +0530

    :memo: Added next.js pages

---
+++ pages/thank-you.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const ThankYou: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Thank You</title>
+        <meta name="description" content="Your technical partner towards web success" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Thank you</h1>
+    </>
+  );
+};
+
+export default ThankYou;
