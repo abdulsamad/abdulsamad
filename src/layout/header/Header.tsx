8d7fd0dd03f41b496c9b3b169d3a5757381fa045
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-08-29 11:38:56 +0530

    :lipstick: Improved header styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
 const Header = styled.header`
+  height: 60px;
   width: 100%;
-  margin: 1rem 0;
+  padding: 1rem 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
   box-sizing: border-box;
   background: transparent;
-  backdrop-filter: blur(60px);
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   z-index: 50;
+  backdrop-filter: blur(30px);
 `;
 
 export default Header;
