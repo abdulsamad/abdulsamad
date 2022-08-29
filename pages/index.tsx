9774c9163a02acc287bed5ca09b47312d8375b71
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-08-29 09:36:56 +0530

    :coffin: Removed some components from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,9 +58,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Hero />
-      <Skills />
-      <Testimonials />
       <Contact />
     </>
   );
