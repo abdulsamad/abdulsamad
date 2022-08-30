14c6ab365ffdb69eb76d42647c296ffefa79c01b
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-08-30 15:04:26 +0530

    :lipstick: Reduced projects padding

--- src/layout/projects/Projects.tsx
+++ src/layout/projects/Projects.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Projects = styled.section`
   background-color: ${({ theme }) => theme.color.white};
-  padding: 5rem 2rem;
+  padding: 2rem 2rem;
   scroll-margin-top: 60px;
 `;
 
