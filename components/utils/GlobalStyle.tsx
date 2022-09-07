2c59a3bf6c6d6ea94d8ba8fa294abb87798c3863
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-07 09:03:51 +0530

    :lipstick: Optimized fonts

--- components/utils/GlobalStyle.tsx
+++ components/utils/GlobalStyle.tsx
@@ -12,8 +12,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0; 
     color: ${({ theme }) => theme.color.text};
-    font-family: ${({ theme }) =>
-      theme.fontFamily.primary}, Arial, Helvetica, Sans-serif;
+    font-family: ${({ theme }) => theme.fontFamily.primary};
   }
 
   ::-webkit-scrollbar {
--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -33,10 +33,15 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
           />
         </Head>
         <body>
