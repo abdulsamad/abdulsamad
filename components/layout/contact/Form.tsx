ae688ae6ad0dff877a0247ed3dd6a7e29e4035ef
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-08 12:28:48 +0530

    :iphone: Improved media query in contact form

--- components/layout/contact/Form.tsx
+++ components/layout/contact/Form.tsx
@@ -7,7 +7,7 @@ const Form = styled.form`
   max-width: 700px;
   padding: 2rem;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     padding: 0;
     width: 100%;
   }
