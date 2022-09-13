8d0e68f5f714c09e3e7bcd2cdb363b36ae248524
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-13 23:28:37 +0530

    :dizzy: Added menu animation

--- components/layout/header/Navbar.tsx
+++ components/layout/header/Navbar.tsx
@@ -1,22 +1,22 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-const Navbar = styled.nav<{ menuOpen: boolean }>`
+const Navbar = styled(motion.nav)`
   display: flex;
   justify-content: flex-end;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    display: ${({ menuOpen }) => (menuOpen ? 'flex' : 'none')};
     flex-direction: column;
     justify-content: center;
     align-items: center;
     position: absolute;
     top: 70px;
     right: 0;
-    transition: 10s ease-in-out;
-    opacity: ${({ menuOpen }) => (menuOpen ? 1 : 0)};
     left: 0;
-    background: ${({ theme }) => theme.color.primary};
-    color: ${({ theme }) => theme.color.light};
+    background: ${({ theme }) => theme.color.white};
+    color: ${({ theme }) => theme.color.text};
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.4);
+    transform-origin: top;
   }
 `;
 
--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 
 import Header from './Header';
@@ -8,9 +7,29 @@ import NavLink from './NavLink';
 import Logo from './Logo';
 import Container from '../../utils/Container';
 import HamburgerBtn from './HamburgerBtn';
+import theme from '../theme';
 
 const Index = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setMatches] = useState(false);
+
+  const query = useRef(`(max-width: ${theme.breakpoints.sm})`);
+
+  useEffect(() => {
+    const media = window.matchMedia(query.current);
+
+    if (media.matches !== isMobile) {
+      setMatches(media.matches);
+    }
+
+    const listener = () => setMatches(media.matches);
+
+    // Listen to Resize Event
+    window.addEventListener('resize', listener);
+
+    // Remove Resize Event on unmount
+    return () => window.removeEventListener('resize', listener);
+  }, [isMobile]);
 
   const toggleMenu = () => setMenuOpen((prevState) => !prevState);
 
@@ -18,12 +37,17 @@ const Index = () => {
     <Header>
       <Container>
         <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}>
+          initial={{ opacity: 0, scale: 0.3 }}
+          animate={{ opacity: 1, scale: 1 }}>
           <Logo height={40} />
         </motion.div>
-        <NavMenu menuOpen={menuOpen}>
+        <NavMenu
+          animate={menuOpen ? 'open' : 'closed'}
+          initial={{ scaleY: isMobile ? 0 : 1 }}
+          variants={{
+            open: { scaleY: 1 },
+            closed: { scaleY: isMobile ? 0 : 1 },
+          }}>
           <NavLink href="#skills">Skills</NavLink>
           <NavLink href="#projects">Projects</NavLink>
           <NavLink href="#contact">Contact</NavLink>
