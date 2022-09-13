21eba9edb87b9c515cee69e4e3f933964e39c049
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-13 12:00:46 +0530

    :lipstick: Improved thank you page with svg

---
+++ components/layout/thank-you/MessageContainer.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const MessageContainer = styled.div`
+  margin: 1rem 0;
+`;
+
+export default MessageContainer;
--- components/layout/thank-you/index.tsx
+++ components/layout/thank-you/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
+import ThankYou from './ThankYou';
 import Container from '../../utils/Container';
 import { Heading1, Heading5 } from '../../utils/Heading';
+import ImageContainer from './ImageContainer';
+import MessageContainer from './MessageContainer';
+import Button from '../../utils/Button';
 
 const Index = () => {
+  const router = useRouter();
+
   return (
-    <Container direction="column" justify="center">
-      <Heading1>Thank you</Heading1>
-      <br />
-      <Heading5>Your message has been sent.</Heading5>
-    </Container>
+    <ThankYou>
+      <Container direction="column" justify="center">
+        <ImageContainer>
+          <Image
+            height={180}
+            width={150}
+            layout="fixed"
+            src="/Mail.svg"
+            alt="Image sent"
+          />
+        </ImageContainer>
+        <Heading1>Thank you</Heading1>
+        <MessageContainer>
+          <Heading5>Your message has been sent.</Heading5>
+        </MessageContainer>
+        <Button onClick={() => router.back()} outlined>
+          Back to Site
+        </Button>
+      </Container>
+    </ThankYou>
   );
 };
 
