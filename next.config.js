ac3ee2c7fde4b89a385cad54c9c569f18a91240a
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-15 11:56:20 +0530

    :wrench: Removed styled components from config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
   images: {
     domains: [
       'repository-images.githubusercontent.com',
