e97ed21f2ebc410523781908978a76940bd196f6
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-12 17:02:23 +0530

    :label: Added types for environment vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PUBLIC_HCAPTCHA_SITE_KEY: string;
+    HCAPTCHA_SITE_SECRET: string;
+    GITHUB_ACCESS_TOKEN: string;
+  }
+}
+
+declare namespace Express {
+  export interface Request {
+    token: string;
+  }
+}
