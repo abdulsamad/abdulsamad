10048d320bc30f7618e8866f87cbab077fb3930b
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-01 10:30:37 +0530

    :wrench: Updated tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,57 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#F2F0FF',
+          100: '#E6E1FE',
+          200: '#CCC3FD',
+          300: '#AEA1FD',
+          400: '#9583FC',
+          500: '#7C65FB',
+          600: '#401FF9',
+          700: '#2305CC',
+          800: '#18048B',
+          900: '#0C0246',
+          950: '#060123',
+          DEFAULT: '#7c65fb',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'Open Sans', 'sans-serif'],
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      backgroundImage: {
+        gradient: 'linear-gradient(45deg, #7c65fb, #da62c4 30%, #7c65fb 60%)',
+      },
+      lineHeight: {
+        12: '3rem',
+        14: '3.5rem',
+      },
+      animation: {
+        'text-gradient': 'text-gradient 10s linear infinite',
+      },
+      keyframes: {
+        'text-gradient': {
+          to: {
+            'background-position': '400%',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
