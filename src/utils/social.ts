12feba6e950cbd72c9944c746f7034864805dedf
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-02 00:39:25 +0530

    :speech_balloon: Added twitter social link

--- src/utils/social.ts
+++ src/utils/social.ts
@@ -1,6 +1,7 @@
 export const socialLinks = {
   linkedIn: 'https://www.linkedin.com/in/abdulsamad-ansari',
   github: 'https://github.com/abdulsamad',
+  twitter: 'https://twitter.com/AbdulSamadDev',
 };
 
 export default socialLinks;
