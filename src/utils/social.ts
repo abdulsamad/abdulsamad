bb68b44d08fbe781412fb11773bb7c006c31c73a
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-02 17:25:02 +0530

    :wrench: Moved utils and update tsconfig

--- src/utils/social.ts
+++
@@ -1,7 +0,0 @@
-export const socialLinks = {
-  linkedIn: 'https://www.linkedin.com/in/abdulsamad-ansari',
-  github: 'https://github.com/abdulsamad',
-  twitter: 'https://twitter.com/AbdulSamadDev',
-};
-
-export default socialLinks;
