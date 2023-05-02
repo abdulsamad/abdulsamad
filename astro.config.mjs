bc60988c929026052073c6cf91bfeb6068f2d8eb
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-03 00:05:03 +0530

    :heavy_plus_sign: Added astro images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="astro/client" />
+/// <reference types="astro/image/client" />
 
 interface ImportMetaEnv {
   readonly PUBLIC_HCAPTCHA_SITE_KEY: string;
