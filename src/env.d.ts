e39346868e1dc9f07213f7d08bc0d28c7dfb2cf7
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-02 20:02:06 +0530

    :fire: Removed unwanted comment

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly PUBLIC_HCAPTCHA_SITE_KEY: string;
   readonly HCAPTCHA_SITE_SECRET: string;
   readonly GITHUB_ACCESS_TOKEN: string;
-  // more env variables...
 }
 
 interface ImportMeta {
