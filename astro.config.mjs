f341ceb8331670033510da037fc233ee2693eab9
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-02 18:04:31 +0530

    :wrench: Added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()],
 });
