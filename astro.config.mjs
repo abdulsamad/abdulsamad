e1e0d7a183d39de364f87fe6e288ac09ac1e2a19
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-09 14:38:43 +0530

    :wrench: Removed SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
 import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   integrations: [tailwind(), image()],
 });
