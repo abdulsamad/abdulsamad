7e557f083849bf00fd8ff98ff2ed6b24b308dc30
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-09 14:39:02 +0530

    :up: Updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   integrations: [tailwind(), image()],
