d0452e6cccf3bea801d04c21e8da3c0a5f0b0365
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-05-10 14:45:36 +0530

    :construction: Added hcaptcha serverless function

--- functions/hcaptcha/hcaptcha.ts
+++ functions/hcaptcha/hcaptcha.ts
@@ -1,83 +1,30 @@
 import type { Handler } from '@netlify/functions';
-import fetch from 'node-fetch';
+import { verify } from 'hcaptcha';
 
-import { githubPinnedReposQuery, technologies } from '@utils/index';
-import type { GitHubPinnedReposType } from '@utils/types';
-import fallbackProjects from '@utils/fallback-projects.json';
-
-const handler: Handler = async (event, context) => {
+const handler: Handler = async (event) => {
   try {
     //  Check body contains data
-    // if (!event.body) throw new Error('No data found in body!');
+    if (!event.body) throw new Error('No data found in body!');
 
-    const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+    const { token } = JSON.parse(event.body);
+    const secret = process.env.HCAPTCHA_SITE_SECRET;
 
-    // No access token found
-    if (!GITHUB_ACCESS_TOKEN) throw new Error('No access token found!');
+    // No token found
+    if (!token) throw new Error('No token found!');
 
-    // Headers
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${GITHUB_ACCESS_TOKEN}`,
-    };
-
-    // Request
-    const res = await fetch('https://api.github.com/graphql', {
-      method: 'POST',
-      headers,
-      body: githubPinnedReposQuery,
-    });
-    const data = ((await res.json()) as any).data as GitHubPinnedReposType;
-    const filteredProjects = data.user.pinnedItems.edges.map(
-      ({
-        node: {
-          description,
-          homepageUrl,
-          id,
-          name,
-          openGraphImageUrl,
-          url,
-          repositoryTopics,
-        },
-      }) => {
-        // Cleans the topics
-        const cleanedTopics = repositoryTopics.edges.map(
-          ({
-            node: {
-              topic: { name },
-            },
-          }) => {
-            return name;
-          }
-        );
-
-        // Filter the topics
-        const topics = cleanedTopics.filter((name: string) =>
-          technologies.includes(name)
-        );
-
-        return {
-          homepageUrl,
-          description,
-          name: name.split('-').join(' '),
-          id,
-          url,
-          openGraphImageUrl,
-          topics,
-        };
-      }
-    );
+    // Verify the hCaptcha token
+    const res = await verify(secret, token);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(filteredProjects),
+      body: JSON.stringify(res),
     };
   } catch (err) {
     const errMsg = err instanceof Error ? err.message : 'Something went wrong!';
 
     return {
       statusCode: 500,
-      body: JSON.stringify({ fallbackProjects }),
+      body: JSON.stringify({ err: errMsg }),
     };
   }
 };
