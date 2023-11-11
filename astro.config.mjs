06ddb85dd2f833d7563bda68d5ce7a797427a557
Abdul Samad Ansari <hello@abdulsamad.dev>
2023-11-11 14:09:57 +0530

    feat: Added Qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
+import qwikdev from '@qwikdev/astro';
 
+// https://astro.build/config
 export default defineConfig({
   root: '.',
   output: 'static',
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), qwikdev()],
   server: {
     port: 8888,
   },
