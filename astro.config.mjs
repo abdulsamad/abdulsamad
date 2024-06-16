6bb5a34dc31ffe29fc1f519c7c623c803fd31471
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-06-16 15:24:44 +0530

    fix: build failed after astro upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import qwikdev from '@qwikdev/astro';
+import icon from 'astro-icon';
 
+// https://astro.build/config
 export default defineConfig({
   root: '.',
   output: 'static',
-  integrations: [tailwind(), qwikdev()],
-  server: { port: 8888 },
+  integrations: [icon(), tailwind(), qwikdev()],
+  server: {
+    port: 8888,
+  },
   image: {
-    remotePatterns: [{ protocol: 'https' }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+      },
+    ],
     domains: ['githubassets.com'],
   },
 });
