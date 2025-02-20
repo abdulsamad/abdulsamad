1c5badea3284dc65385e52f429529a59c4bc7c78
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-02-20 11:57:35 +0530

    refactor: change pinned repos API

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PINNED_REPOS_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
