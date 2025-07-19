6647c2f384637a954521100daf53b57226974623
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-07-20 00:46:47 +0530

    refactor: change posthog ui path

--- src/posthog.js
+++ src/posthog.js
@@ -6,6 +6,7 @@ export default function PostHogClient() {
   if (!posthogClient) {
     posthogClient = new PostHog(import.meta.env.PUBLIC_POSTHOG_KEY, {
       host: import.meta.env.PUBLIC_POSTHOG_HOST,
+      ui_host: 'https://us.posthog.com',
     });
   }
 
