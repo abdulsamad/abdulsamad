88f9cf81b432710b9dbc2dc8ba1ae592fc451013
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-07-19 21:17:43 +0530

    fix: public posthog key

--- src/posthog.js
+++ src/posthog.js
@@ -5,7 +5,7 @@ let posthogClient = null;
 export default function PostHogClient() {
   if (!posthogClient) {
     posthogClient = new PostHog(import.meta.env.PUBLIC_POSTHOG_KEY, {
-      host: 'https://us.i.posthog.com',
+      host: import.meta.env.PUBLIC_POSTHOG_HOST,
     });
   }
 
