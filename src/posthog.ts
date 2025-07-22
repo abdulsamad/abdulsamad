49478f5484bb9989478bb28294ef31a32a2e60bd
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-07-22 22:12:18 +0530

    chore: upgraded dependencies

--- src/posthog.ts
+++ src/posthog.ts
@@ -1,12 +1,11 @@
 import { PostHog } from 'posthog-node';
 
-let posthogClient = null;
+let posthogClient: PostHog | null = null;
 
 export default function PostHogClient() {
   if (!posthogClient) {
     posthogClient = new PostHog(import.meta.env.PUBLIC_POSTHOG_KEY, {
       host: import.meta.env.PUBLIC_POSTHOG_HOST,
-      ui_host: 'https://us.posthog.com',
     });
   }
 
