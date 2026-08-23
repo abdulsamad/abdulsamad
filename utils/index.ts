fe199adff4e708379ecc013003a62cdc537fc5ab
Abdul Samad Ansari <hello@abdulsamad.dev>
2026-08-23 10:10:21 +0530

    Implement structural updates and optimizations across multiple modules

--- utils/index.ts
+++ utils/index.ts
@@ -1,23 +1,17 @@
-export const socialLinks = [
-  { name: 'LinkedIn', iconName: 'linkedin', url: 'https://www.linkedin.com/in/abdulsamad-ansari' },
-  { name: 'GitHub', iconName: 'github', url: 'https://github.com/abdulsamad' },
-  { name: 'Twitter', iconName: 'twitter', url: 'https://twitter.com/AbdulSamadDev' },
-];
-
 export const initialProdLog = () => {
   // prettier-ignore
   if (process.env.NODE_ENV === 'production') {
-			const txt = '%c This website is developed by AbdulSamad. Hopefully there are no error and warnings in console! 😄';
-			const art = `%c
+    const txt = '%c This website is developed by AbdulSamad. Hopefully there are no error and warnings in console! 😄';
+    const art = `%c
 		___   _____   _____   _   _   _       _____       ___       ___  ___       ___   _____  
 		/   | |  _  \\ |  _  \\ | | | | | |     /  ___/     /   |     /   |/   |     /   | |  _  \\ 
 		/ /| | | |_| | | | | | | | | | | |     | |___     / /| |    / /|   /| |    / /| | | | | | 
 		/ /_| | |  _  { | | | | | | | | | |     \\___  \\   / /_| |   / / |__/ | |   / /_| | | | | | 
 		/ /  | | | |_| | | |_| | | |_| | | |___   ___| |  / /  | |  / /       | |  / /  | | | |_| | 
 		/_/   |_| |_____/ |_____/ \\_____/ |_____| /_____/ /_/   |_| /_/        |_| /_/   |_| |_____/ `;
-
-			console.log(art, 'font-weight: bold; color: #2f89fc;');
-			console.log(txt, 'font-size: 16px; font-weight: 600; text-shadow: 1px 1px 2px #c4c4c4,1px 1px 2px #d3d3d3; margin: 5px 0;');
+    
+    console.log(art, 'font-weight: bold; color: #2f89fc;');
+    console.log(txt, 'font-size: 16px; font-weight: 600; text-shadow: 1px 1px 2px #c4c4c4,1px 1px 2px #d3d3d3; margin: 5px 0;');
 	}
 };
 
@@ -128,4 +122,10 @@ export const redirects = {
     status: 301,
     destination: 'https://bsky.app/profile/abdulsamad.bsky.social',
   },
-};
+} as const;
+
+export const socialLinks = [
+  { name: 'LinkedIn', iconName: 'linkedin', url: redirects['/linkedin'].destination },
+  { name: 'GitHub', iconName: 'github', url: redirects['/github'].destination },
+  { name: 'Twitter', iconName: 'twitter', url: redirects['/twitter'].destination },
+] as const;
